8e9fd74dc3a3bdab33c00df92331d02d55a641f8
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-04-26 11:41:08 +0430

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -2,6 +2,5 @@ from . import *
 from telethon import custom, events
 
 @bot_cmd("start", is_args=False)
-@god_only
 async def _(event):
     await event.reply("<b><u>I'm Online</b></u>", parse_mode="HTML")
