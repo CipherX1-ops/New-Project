e328c0122d04e03103a27ab439cfecabeb8b7891
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-04-26 10:47:20 +0430

    Update Config.py

--- bot/Config.py
+++ bot/Config.py
@@ -3,7 +3,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    COMMAND_HAND_LER = os.environ.get("HANDLER", "^/")
-    DB_URL = os.environ.get("DATABASE_URL", None)
+    HANDLER = os.environ.get("HANDLER", "^/")
+    DB_URL = os.environ.get("DB_URL", None)
     OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
